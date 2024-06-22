fb36fffeab05b0e3fa33ee4abbc4d998d2b02bc3
Lakshya Mahawar <83651022+lakshyamahawar14@users.noreply.github.com>
2024-06-22 21:56:19 +0530

    updating full screen behaviour

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,11 @@ import 'local.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive, overlays: []);
+
+  // Ensure the app starts and remains in full-screen mode
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
+  // Set preferred orientations to landscape
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeRight,
     DeviceOrientation.landscapeLeft,
@@ -16,67 +20,28 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const FullScreenWidget(
-              child: Scaffold(
-                backgroundColor: Colors.white,
-                body: HomePage(),
-              ),
-            ),
-        '/multiplayer': (context) => const Multiplayer(),
-        '/local': (context) => const Local(),
-      },
-    );
-  }
-}
-
-class FullScreenWidget extends StatefulWidget {
-  final Widget child;
-
-  const FullScreenWidget({super.key, required this.child});
-
-  @override
-  _FullScreenWidgetState createState() => _FullScreenWidgetState();
-}
-
-class _FullScreenWidgetState extends State<FullScreenWidget> with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-    _enableFullScreen();
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-    }
-  }
+  const MyApp({Key? key}) : super(key: key);
 
   void _enableFullScreen() {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive, overlays: []);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        _enableFullScreen();
-      },
-      child: widget.child,
+      onTap: _enableFullScreen,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const Scaffold(
+                backgroundColor: Colors.white,
+                body: HomePage(),
+              ),
+          '/multiplayer': (context) => const Multiplayer(),
+          '/local': (context) => const Local(),
+        },
+      ),
     );
   }
 }
